e9f71373bd3a7d679d3486d373bc9b81b9ab1a68
Artem Krasavin <arkrasavin.ak@gmail.com>
2023-09-18 10:47:00 +0300

    Правка

--- utils/storage.py
+++ utils/storage.py
@@ -5,7 +5,7 @@ import re
 def check_suffix(separation_nickname_url, short_url, path):
     with (open(path, 'r', encoding='utf-8') as file):
         read_file = json.load(file)
-    for i in read_file.keys():
-        if (separation_nickname_url in re.findall(r'\S+(?=/)', i) and
-                re.findall(r'(?=/)\S+', i) == re.findall(r'(?=/)\S+', short_url)):
+    for i_key in read_file.keys():
+        if (separation_nickname_url in re.findall(r'\S+(?=/)', i_key) and
+                re.findall(r'(?=/)\S+', i_key) == re.findall(r'(?=/)\S+', short_url)):
             return True
